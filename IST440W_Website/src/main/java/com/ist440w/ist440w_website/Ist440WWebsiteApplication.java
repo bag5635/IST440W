8379d66292ae386345304164e9048df70821cb74
bag5635 <85423611+bag5635@users.noreply.github.com>
2022-08-29 10:36:46 -0400

    Add files via upload

---
+++ IST440W_Website/src/main/java/com/ist440w/ist440w_website/Ist440WWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ist440w.ist440w_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ist440WWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ist440WWebsiteApplication.class, args);
+    }
+
+}
---
+++ IST440W_Website/src/test/java/com/ist440w/ist440w_website/Ist440WWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ist440w.ist440w_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ist440WWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
