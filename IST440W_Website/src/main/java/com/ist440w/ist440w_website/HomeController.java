1ff67235102b0d39abf95a81c55d4c2109fbe471
Brian Gorbea <bag635@psu.edu>
2022-09-18 15:01:36 -0400

    Page interconnection with spring and local testing.

--- IST440W_Website/src/main/java/com/ist440w/ist440w_website/HomeController.java
+++ IST440W_Website/src/main/java/com/ist440w/ist440w_website/HomeController.java
@@ -2,11 +2,21 @@ package com.ist440w.ist440w_website;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/About_Us.html")
+    public String aboutus() {
+        return "About_Us";
+    }
+    @RequestMapping("/index.html")
+    public String home() {
         return "index";
     }
 
