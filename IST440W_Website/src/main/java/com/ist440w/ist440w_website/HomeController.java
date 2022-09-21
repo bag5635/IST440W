f04e30d69871aaf110f3e8b80da3f634ecbf37ac
Brian Gorbea <bag635@psu.edu>
2022-09-21 14:54:36 -0400

    Pups, and href for all pages so far.

--- IST440W_Website/src/main/java/com/ist440w/ist440w_website/HomeController.java
+++ IST440W_Website/src/main/java/com/ist440w/ist440w_website/HomeController.java
@@ -23,4 +23,10 @@ public class HomeController {
     @RequestMapping("/Contact_Us.html")
     public String contactus(){return "Contact_Us";}
 
+    @RequestMapping("/Current_Pups.html")
+    public String cur_pup(){return "Current_Pups";}
+
+    @RequestMapping("/Old_Puppies.html")
+    public String old_pup(){return "Old_Puppies";}
+
 }
